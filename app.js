d38b4372a138aebc8444e09de8609ad4d97e5ba3
Luis Lopez <luchoalopez91@gmail.com>
2023-02-04 23:53:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+let app = express();
+const PORT = 3000;
+const path = require('path')
+
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    return res.sendFile(path.join(__dirname,"/views/index.html"))
+})
+
+app.listen(PORT, () => console.log(`El servidor esta escuchando en el
+puerto ${PORT}
+http://localhost:${PORT}`));
